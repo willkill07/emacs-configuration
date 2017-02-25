b7ddf313549bc898a7e4cc1a3a1651a295dc437f
Will Killian <william.killian@gmail.com>
2017-02-25 17:22:23 -0500

    Added a bunch, but disabled extra language support

--- init.el
+++ init.el
@@ -25,6 +25,8 @@
 (setq inhibit-startup-screen t)
 (defalias 'yes-or-no-p 'y-or-n-p)
 
+;; Interface
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
@@ -37,7 +39,17 @@
 (setq mouse-wheel-progressive-speed nil)
 (setq mouse-wheel-follow-mouse 't)
 (setq scroll-step 1)
+
+;; Tab and whitespace hell
+
+(setq-default indent-tabs-mode nil)
+(defun untabify-except-makefiles ()
+  "Replace tabs with spaces except in makefiles."
+  (unless (derived-mode-p 'makefile-mode)
+    (untabify (point-min) (point-max))))
+
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
+(add-hook 'before-save-hook 'untabify-except-makefiles)
 
 ;; Package loading
 (use-package exec-path-from-shell
@@ -49,7 +61,7 @@
 (use-package smooth-scrolling
   :ensure t
   :config
-  (setq smooth-scroll-margin 5))
+  (setq smooth-scroll-margin 8))
 
 (use-package powerline
   :ensure t
@@ -84,10 +96,31 @@
   (git-gutter:linum-setup)
   (global-git-gutter-mode))
 
+(use-package tramp
+  :config
+  (setq tramp-verbose        9
+        tramp-default-method "ssh"
+        tramp-ssh-controlmaster-options
+        (concat "-o ControlPath=/tmp/tramp.%%r@%%h:%%p "
+                "-o ControlMaster=auto "
+                "-o ControlPersist=no")))
+
+(use-package gitconfig-mode
+  :ensure t
+  :mode ("/\\.gitconfig\\'" "/\\.git/config\\'" "/git/config\\'" "/\\.gitmodules\\'"))
+
+(use-package gitignore-mode
+  :ensure t
+  :mode ("/\\.gitignore\\'" "/\\.git/info/exclude\\'" "/git/ignore\\'"))
+
+(use-package cmake-mode
+  :ensure t
+  :mode ("CMakeLists\\.txt\\'" "\\.cmake\\'"))
+
 (use-package modern-cpp-font-lock
   :ensure t
   :diminish modern-c++-font-lock-mode
-  :config (add-hook 'c++-mode-hook 'modern-c++-font-lock-mode))
+  :init (add-hook 'c++-mode-hook 'modern-c++-font-lock-mode))
 
 (use-package company
   :ensure t
@@ -100,7 +133,6 @@
 
 (use-package flycheck
   :ensure t
-  :defer 1
   :diminish flycheck-mode "F "
   :config
   (global-flycheck-mode)
@@ -108,56 +140,142 @@
 
 (use-package irony
   :ensure t
-  :defer 3
+  :after company
+  :diminish irony-mode "I "
   :init
   (add-hook 'c-mode-hook 'irony-mode)
   (add-hook 'c++-mode-hook 'irony-mode)
   :config
-  (defun my-irony-mode-hook ()
-    (define-key irony-mode-map [remap completion-at-point] 'irony-completion-at-point-async)
-    (define-key irony-mode-map [remap complete-symbol] 'irony-completion-at-point-async))
-  (add-hook 'irony-mode-hook 'my-irony-mode-hook)
-  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-  (if (not (irony--locate-server-executable))
-    (irony-install-server
-     (format
-      (concat "%s %s %s && %s --build . --use-stderr --config Release --target install")
-      (shell-quote-argument irony-cmake-executable)
-      (shell-quote-argument
-       (concat "-DCMAKE_INSTALL_PREFIX=" (expand-file-name irony-server-install-prefix)))
-      (shell-quote-argument irony-server-source-dir)
-      (shell-quote-argument irony-cmake-executable))))
+  (progn
+    (defun my-irony-mode-hook ()
+      (define-key irony-mode-map [remap completion-at-point] 'irony-completion-at-point-async)
+      (define-key irony-mode-map [remap complete-symbol] 'irony-completion-at-point-async))
+    (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+    (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+    (if (not (irony--locate-server-executable))
+        (irony-install-server
+         (format
+          (concat "%s %s %s && %s --build . --use-stderr --config Release --target install")
+          (shell-quote-argument irony-cmake-executable)
+          (shell-quote-argument
+           (concat "-DCMAKE_INSTALL_PREFIX=" (expand-file-name irony-server-install-prefix)))
+          (shell-quote-argument irony-server-source-dir)
+          (shell-quote-argument irony-cmake-executable))))
 
-  (add-hook 'c-mode-hook
-   (lambda()
-     (setq irony-additional-clang-options
-	   (quote ("-Wall" "-Wextra")))))
-  (add-hook 'c++-mode-hook
-   (lambda()
-     (setq irony-additional-clang-options
-	   (quote ("-std=c++14" "-Wall" "-Wextra" "-pedantic-errors")))))
+    (add-hook 'c-mode-hook
+              (lambda()
+                (setq irony-additional-clang-options
+                      (quote ("-Wall" "-Wextra")))))
+    (add-hook 'c++-mode-hook
+              (lambda()
+                (setq irony-additional-clang-options
+                      (quote ("-std=c++14" "-Wall" "-Wextra" "-pedantic-errors")))))))
 
-  (use-package company-irony
-    :ensure t
-    :diminish irony-mode "I "
-    :config
-    (use-package company-irony-c-headers
-      :ensure t))
-  (add-to-list 'company-backends '(company-irony-c-headers company-irony))
+(use-package company-irony
+  :ensure t
+  :after company)
 
-  (use-package flycheck-irony
-    :ensure t
-    :config
-    (eval-after-load 'flycheck
-      '(add-hook 'flycheck-mode-hook 'flycheck-irony-setup))))
+(use-package company-irony-c-headers
+  :ensure t
+  :after company-irony
+  :config (add-to-list 'company-backends '(company-irony-c-headers company-irony)))
+
+(use-package flycheck-irony
+  :ensure t
+  :after flycheck
+  :config (add-hook 'flycheck-mode-hook 'flycheck-irony-setup))
 
 (use-package clang-format
   :ensure t
-  :bind
-  ("C-f" . clang-format-buffer))
+  :bind ("C-f" . clang-format-buffer))
 
 (use-package bison-mode
+  :ensure t
+  :mode ("\\.y\\'" "\\.l\\'"))
+
+;; Assembly / Bytecodes
+
+(use-package autodisass-java-bytecode
+  :disabled
   :ensure t)
+
+(use-package llvm-mode
+  :disabled
+  :ensure t
+  :config
+  (use-package autodisass-llvm-bitcode
+    :ensure t))
+
+(use-package mips-mode
+  :disabled
+  :ensure t)
+
+;; Non-C-Like Languages
+
+(use-package jedi
+  :disabled
+  :ensure t
+  :commands jedi:setup
+  :init (add-hook 'python-mode-hook 'jedi:setup)
+  :config (jedi:install-server))
+
+(use-package company-jedi
+  :disabled
+  :ensure t
+  :after jedi
+  :config (add-to-list 'company-backends 'company-jedi))
+
+;; Plaintext Modes
+
+(use-package markdown-mode
+  :ensure t
+  :mode ("\\.markdown\\'" "\\.mk?d\\'" "\\.text\\'"))
+
+(use-package json-mode
+  :ensure t
+  :mode "\\.json$"
+  :config (setq js-indent-level 2))
+
+(use-package yaml-mode
+  :ensure t
+  :mode "\\.ya?ml\'")
+
+;; Web languages
+
+(use-package less-css-mode
+  :ensure t
+  :mode "\\.less\\'")
+
+(use-package web-mode
+  :ensure t
+  :mode ("\\.css\\'" "\\.phtml\\'" "\\.tpl\\.php\\'" "\\.[agj]sp\\'" "\\.as[cp]x\\'" "\\.mustache\\'" "\\.djhtml\\'" "\\.html?\\'")
+  :config
+  (setq web-mode-markup-indent-offset 2
+        web-mode-css-indent-offset 2
+        web-mode-code-indent-offset 2
+        web-mode-attr-indent-offset 2
+        web-mode-style-padding 1
+        web-mode-script-padding 1
+        web-mode-block-padding 0
+        web-mode-enable-auto-pairing t
+        web-mode-enable-css-colorization t
+        web-mode-enable-block-face t
+        web-mode-enable-current-element-highlight t
+        web-mode-enable-current-column-highlight t)
+  (use-package web-beautify
+    :ensure t)
+  :bind ("C-n" . web-mode-tag-match))
+
+(use-package rainbow-mode
+  :ensure t
+  :after web-mode
+  :diminish rainbow-mode
+  :init (add-hook 'web-mode-hook 'rainbow-mode))
+
+(use-package company-web
+  :ensure t
+  :after company
+  :config (add-to-list 'company-backends 'company-web-html))
 
 (provide 'init)
 ;;; init.el ends here
